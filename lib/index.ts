2c7da84591f5d4f89637954dcf58392b5cc91138
Bread <77288737+riley0122@users.noreply.github.com>
2024-03-24 20:15:53 +0100

    Created a start

---
+++ lib/index.ts
@@ -0,0 +1,21 @@
+/**
+ * @property {string} language - The language that the string should be highlighted as, Default is javascript.
+ * @property {string} tailwind - Wether or not to use Tailwind CSS, Default is false.
+ */
+export interface SyntaxHighlighterOptions {
+    language?: string;
+    tailwind?: boolean;
+}
+
+/**
+ * Highlight code in html syntax
+ * @param code The code to be highlighted
+ * @param options Options for highlighting
+ */
+export default function(code: string, options: SyntaxHighlighterOptions) {
+    // Set defualts if not set.
+    options.language = options.language || "javascript";
+    options.tailwind = options.tailwind || false;
+
+    return code;
+}
