0ff1165ab72f69eb17962787f4ffa5aa0e6fc11e
Bread <77288737+riley0122@users.noreply.github.com>
2024-03-24 20:47:39 +0100

    Added color interface

--- lib/index.ts
+++ lib/index.ts
@@ -8,6 +8,25 @@ export interface SyntaxHighlighterOptions {
 }
 
 /**
+ * The colors for the syntax highlighter.
+ * Colors can be in any format css supports when not using tailwind.
+ * When using tailwind it can be any tailwind color found at https://tailwindcss.com/docs/text-color without the "text-" part.
+ * View what types are what on https://en.wikipedia.org/wiki/Lexical_analysis#Lexical_token_and_lexical_tokenization
+ */
+export interface HighlighterColors {
+    identifier?: string;
+    keyword?: string;
+    separator?: string;
+    operator?: string;
+    literals?: {
+        boolean?: string;
+        number?: string;
+        string?: string;
+    };
+    comment?: string;
+}
+
+/**
  * Highlight code in html syntax
  * @param code The code to be highlighted
  * @param options Options for highlighting
