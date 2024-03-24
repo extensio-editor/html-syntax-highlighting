8b83c320fc4371939c0959b38fba89dc9558ddfc
Bread <77288737+riley0122@users.noreply.github.com>
2024-03-24 21:02:19 +0100

    Added colors

--- lib/index.ts
+++ lib/index.ts
@@ -5,6 +5,7 @@
 export interface SyntaxHighlighterOptions {
     language?: string;
     tailwind?: boolean;
+    colors?: HighlighterColors;
 }
 
 /**
@@ -14,17 +15,34 @@ export interface SyntaxHighlighterOptions {
  * View what types are what on https://en.wikipedia.org/wiki/Lexical_analysis#Lexical_token_and_lexical_tokenization
  */
 export interface HighlighterColors {
-    identifier?: string;
-    keyword?: string;
-    separator?: string;
-    operator?: string;
-    literals?: {
-        boolean?: string;
-        number?: string;
-        string?: string;
+    identifier: string;
+    keyword: string;
+    separator: string;
+    operator: string;
+    literals: {
+        boolean: string;
+        number: string;
+        string: string;
     };
-    comment?: string;
+    comment: string;
 }
+
+/**
+ * The default color scheme for the syntax highlighter.
+ * Values are picked generically based on what we thought looked good.
+ */
+const defaultColors: HighlighterColors = {
+    identifier: '#80dbdd',
+    keyword: '#7e43d1',
+    separator: '#d4d4d4',
+    operator: '#7ce6ba',
+    literals: {
+        boolean: '#1a12b3',
+        number: '#3d73f2',
+        string: '#f5a973',
+    },
+    comment: '#0f4503',
+};
 
 /**
  * Highlight code in html syntax
@@ -35,6 +53,7 @@ export default function(code: string, options: SyntaxHighlighterOptions) {
     // Set defualts if not set.
     options.language = options.language || "javascript";
     options.tailwind = options.tailwind || false;
+    options.colors = options.colors || defaultColors;
 
     return code;
 }
