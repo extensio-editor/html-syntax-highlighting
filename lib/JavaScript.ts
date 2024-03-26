34de99cd2e71f4b161fca9df8a3523dffbc24635
Bread <77288737+riley0122@users.noreply.github.com>
2024-03-26 16:01:18 +0100

    Added JavaScrupt language file

---
+++ lib/JavaScript.ts
@@ -0,0 +1,153 @@
+import { registerLanguage, LanguageDefinition } from "./languageManager.js";
+
+const definition: LanguageDefinition = {
+identifier: [/[$a-zA-Z_][a-zA-Z0-9_]*/g],
+    keyword: [
+        /abstract/g,
+        /boolean/g,
+        /break/g,
+        /byte/g,
+        /case/g,
+        /catch/g,
+        /char/g,
+        /class/g,
+        /const/g,
+        /continue/g,
+        /debugger/g,
+        /default/g,
+        /delete/g,
+        /do/g,
+        /double/g,
+        /else/g,
+        /enum/g,
+        /export/g,
+        /extends/g,
+        /false/g,
+        /final/g,
+        /finally/g,
+        /float/g,
+        /for/g,
+        /function/g,
+        /goto/g,
+        /if/g,
+        /implements/g,
+        /import/g,
+        /in/g,
+        /instanceof/g,
+        /int/g,
+        /interface/g,
+        /long/g,
+        /native/g,
+        /new/g,
+        /null/g,
+        /package/g,
+        /private/g,
+        /protected/g,
+        /public/g,
+        /return/g,
+        /short/g,
+        /static/g,
+        /synchronized/g,
+        /switch/g,
+        /this/g,
+        /throw/g,
+        /throws/g,
+        /transient/g,
+        /true/g,
+        /try/g,
+        /typeof/g,
+        /var/g,
+        /void/g,
+        /while/g,
+        /with/g
+    ],
+    separator: [
+        /\}/,
+        /\]/,
+        /\)/,
+        /\>/,
+        /\;/,
+        /\{/,
+        /\[/,
+        /\(/,
+        /\</,
+        /,/
+    ],
+    operator: [
+        /\+/,
+        /\-/,
+        /\*/,
+        /\*\*/,
+        /\//,
+        /\%/,
+        /\+\+/,
+        /\-\-/,
+        /\=/,
+        /\+\=/,
+        /\-\=/,
+        /\*\=/,
+        /\/\=/,
+        /\%\=/,
+        /\*\*\=/,
+        /\=\=/,
+        /\=\=\=/,
+        /\!\=/,
+        /\!\=\=/,
+        /\>/,
+        /\</,
+        /\>\=/,
+        /\<\=/,
+        /\?/,
+        /\:/,
+        /\&\&/,
+        /\|\|/,
+        /\!/,
+        /\&/,
+        /\|/,
+        /\~/,
+        /\^/,
+        /\<\</,
+        /\>\>/,
+        /\>\>\>/
+    ],
+    literals: {
+        boolean: [/true/g,/false/g],
+        number: [/[0-9]/g],
+        string: [
+            /'.*'/g,
+            /".*"/g,
+            /`.*`/g
+        ],
+        null: [/null/g]
+    },
+    comment: [
+        /\/\*.*\*\//g,
+        /\/\/.*/g
+    ],
+}
+
+// Language definition doesnt differ for
+//  - JS
+//  - JavaScript
+//  - TS
+//  - TypeScript
+
+registerLanguage({
+    name: "JS",
+    defenition: definition,
+});
+
+registerLanguage({
+    name: "JavaScript",
+    defenition: definition,
+});
+
+registerLanguage({
+    name: "TS",
+    defenition: definition,
+});
+
+registerLanguage({
+    name: "TypeScript",
+    defenition: definition,
+});
